25b35e30d03442f6834eb8c5798ebf2df4f99c2b
Vashali <vaishali2016@outlook.com>
2018-08-12 18:06:33 +0100

    displays file name

--- handler.js
+++ handler.js
@@ -2,10 +2,21 @@ const express=require('express')
 const app=express()
 const path=require('path')
 
+const bodyParser=require('body-parser')
+
+app.use( bodyParser.json() )       
+app.use(bodyParser.urlencoded({    
+  extended: true
+}))
+
 app.get('/',function(req, res){
 	res.sendFile(path.resolve(__dirname+'/index.html'))
 })
 
+app.post('/file',function(req,res){
+	res.json(req.body.file)
+})
+
 app.listen(3001, function() {
-console.log("Listening on port 3001")
+	console.log("Listening on port 3001")
 })
